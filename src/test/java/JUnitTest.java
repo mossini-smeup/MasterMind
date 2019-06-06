6a4b2361a9355daf355df0a454af43ac540dd90f
mossini-smeup <moss8991@gmail.com>
2019-06-06 10:39:43 +0200

    Update JUnitTest

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -39,7 +39,6 @@ public class JUnitTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    
      
      @Test
      public void numeroValido(){
